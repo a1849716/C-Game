7cba4bda528eba0e2a293a17f02b5ca5e63a5dc2
Yuqi Xiao <a1849716@adelaide.edu.au>
2022-10-12 13:22:56 +1030

    lmao

--- Tetromino.h
+++ Tetromino.h
@@ -3,6 +3,12 @@
 
 class Teromino{
   private:
+  const int I4_tetromino;
+  const int O_tetromino;
+  const int T_tetromino;
+  const int S_tetromino;
+  const int L_tetromino;
+  const int block3_tetromino;
 
   public:
   
